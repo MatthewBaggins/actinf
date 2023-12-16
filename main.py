09bd11e1157f8e4cd5455e093cb7f0d33b2a9a72
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-12-16 13:09:02 +0100

    switched to using torch.distributions

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import torch as t
 
 from src.envs import make_envs, observe_envs
+from torch.distributions.categorical import Categorical
+
 from src.utils import seed
 from src.thermostat import Thermostat
 
+
 SEED = 42
 N_ENVS = 10
+N_ROUNDS = 100
 
 
 def main() -> None:
     seed(SEED)
     envs = make_envs(N_ENVS)
     model = Thermostat()
-    obs = observe_envs(envs, 0)
-    logits = model(obs)
+    obs = observe_envs(envs, 1)
+    for round_i in range(N_ROUNDS):
+        action_probs = model(obs)
+        actions = model.sample_action(action_probs)
+        print(round_i, action_probs.shape, actions.shape)
+        print(actions)
+        break
 
 
 if __name__ == "__main__":
--- src/thermostat.py
+++ src/thermostat.py
@@ -1,26 +1,46 @@
 import torch as t
 from torch import nn
-from scipy.stats import norm, expon
+from torch.distributions import Categorical, Normal
+
+# from src.stats import pdf_normal
+
+HIDDEN_DIM = 3
 
 
 class Thermostat(nn.Module):
-    def __init__(self, temp_mu: float = 20, hidden_dim: int = 3) -> None:
+    def __init__(self, *, temp_mu: float = 20, temp_sigma: float = 1) -> None:
         super().__init__()
-        self.temp = norm(temp_mu)
-        self.hidden_dim = hidden_dim
+        self.temp_mu = temp_mu
+        self.temp_sigma = temp_sigma
+        self.temp = Normal(temp_mu, temp_sigma)
+
+        self.hidden_dim = HIDDEN_DIM
         self.net = nn.Sequential(
-            nn.Linear(2, hidden_dim), nn.ReLU(), nn.Linear(hidden_dim, 2)
+            nn.Linear(2, HIDDEN_DIM), nn.ReLU(), nn.Linear(HIDDEN_DIM, 2)
         )
 
     def forward(
-        self, obs: t.Tensor  # [2] temp pow1 (fully states are fully observable)
+        self,
+        obs: t.Tensor,  # [2] temp pow1 (fully states are fully observable)
     ) -> t.Tensor:  # [2] logits of action (-1 or 1)
         x = obs
         for layer in self.net:
             x = layer(x)
         return x
-    
+
     def loss_fn(
         self,
-        obs: t.Tensor, 
-    )
+        obs: t.Tensor,  # [2] (temp pow1)
+    ) -> t.Tensor:
+        temp = obs[:, 0]
+        densities = self.temp_densities(temp)
+        loss = -densities.pow(2).mean()
+        return loss
+
+    def temp_densities(self, temp: t.Tensor) -> t.Tensor:
+        return self.temp.log_prob(temp).exp()
+
+    def sample_action(
+        self, action_probs: t.Tensor  # [n_envs n_actions]
+    ) -> t.Tensor:  # [n_envs 1] (row-wise: unit tensor [index of chosen action])
+        return Categorical(action_probs).sample()
