2d174595f4b868ee0c618f9e50ab5c3213957101
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-12-24 06:27:41 +0100

    removed stats stuff

--- src/stats.py
+++
@@ -1,7 +0,0 @@
-import torch as t
-
-# TODO write tests for pdf normal
-
-
-def pdf_normal(x: t.Tensor, *, mu: float, sigma: float) -> t.Tensor:
-    return t.exp(-0.5 * ((x - mu) / sigma) ** 2) / (sigma * (2 * t.pi) ** 0.5)
